50bd52ccf01a556d4c0ebd4543d5808be7308561
ddujeong <qoddl159@nate.com>
2025-09-18 15:55:30 +0900

    first commit

---
+++ js/example.js
@@ -0,0 +1,9 @@
+document.write("helloWorld!\"헬로우 월드!\"");
+var str1 ="kor";
+var str2 = 'jap';
+var num1 = 100;
+var num2 = Number(500); // 숫자 500 -> 숫자 500
+var num3 = Number("500"); // 문자 500 -> 숫자 500
+var num4 = Number("abc"); // 반환 불가 -> NaN
+var tag1 = "<h1> String </h1>"
+var bool1 = Boolean(0); // false -> 0, null, "", undefined가 아닌 값은 모두 true
